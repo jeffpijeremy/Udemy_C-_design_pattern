0aecc937379f3407c10856cb4bbc1e3f5d45e7e9
jeffpijeremy <80880882+jeffpijeremy@users.noreply.github.com>
2023-12-28 16:58:15 +0800

    介面隔離原則
    客戶端不應該被迫依賴於它不使用的接口。接口應該被拆分為更小和更具體的部分，以便客戶端只需要知道它們所需的部分。
    
    在這個新的設計中，我們將原來的介面 Machine 拆成了三個獨立的介面 Printer、Fax 和 Scanner。MultiFunctionDevice 是一個具有多種功能的裝置，它實現了 Printer、Fax 和 Scanner 三個介面。這樣的設計讓每個介面只包含必要的方法，並讓裝置可以實現自己所需要的介面。這樣的設計更符合 ISP。

---
+++ DesignPatterns/Interface Segregation Principle/Program.cs
@@ -0,0 +1,107 @@
+﻿namespace Interface_Segregation_Principle
+{
+    public class Document
+    {
+
+    }
+    public interface IMachine
+    {
+        void Print(Document d);
+        void Scan(Document d);
+        void Fax(Document d);
+    }
+    public class MultiFunctionPrinter : IMachine
+    {
+
+        public void Print(Document d)
+        {
+            
+        }
+
+        public void Scan(Document d)
+        {
+            
+        }
+        public void Fax(Document d)
+        {
+
+        }
+    }
+    public class OldFashionedPrinter : IMachine
+    {
+        public void Print(Document d)
+        {
+
+        }
+
+        public void Scan(Document d)
+        {
+            
+        }
+        public void Fax(Document d)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+    public interface IPrinter
+    {
+        void Print(Document d);
+    }
+    public interface IScanner
+    {
+        void Scan(Document d);
+    }
+    public class Photocopier : IPrinter, IScanner
+    {
+        public void Print (Document d)
+        {
+            throw new NotImplementedException();
+        }
+        public void Scan(Document d)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public interface IMultiFunctionDevice : IScanner, IPrinter
+    {
+
+    }
+    public struct MultiFunctionMachine : IMultiFunctionDevice
+    {
+        // compose this out of several modules
+        private IPrinter printer;
+        private IScanner scanner;
+
+        public MultiFunctionMachine(IPrinter printer, IScanner scanner)
+        {
+            if (printer == null)
+            {
+                throw new ArgumentNullException(paramName: nameof(printer));
+            }
+            if (scanner == null)
+            {
+                throw new ArgumentNullException(paramName: nameof(scanner));
+            }
+            this.printer = printer;
+            this.scanner = scanner;
+        }
+
+        public void Print(Document d)
+        {
+            printer.Print(d);
+        }
+
+        public void Scan(Document d)
+        {
+            scanner.Scan(d);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
