2b895719f8acbd0f2236c7e54d82491a6e66ac36
jeffpijeremy <80880882+jeffpijeremy@users.noreply.github.com>
2023-12-28 16:39:07 +0800

    里式替換原則範例
    所有引用基類別的地方必須能夠透明地使用其子類別的對象。換句話說，子類別應該可以替換其父類別並且不會破壞系統的正確性。
    
    所以改用virtual int 宣告，後續使用者可以用override替換父類別的實作內容，且使其一樣保持父類別的屬性

--- DesignPatterns/Liskov Substitution Principle/Program.cs
+++ DesignPatterns/Liskov Substitution Principle/Program.cs
@@ -2,8 +2,8 @@
 {
     public class Rectangle
     {
-        public int Width { get; set; }
-        public int Height { get; set; }
+        public virtual int Width { get; set; }
+        public virtual int Height { get; set; }
         public Rectangle()
         {
 
@@ -20,11 +20,11 @@
     }
     public class Square : Rectangle
     {
-        public new int Width
+        public override int Width
         {
             set { base.Width = base.Height = value; }
         }
-        public new int Height
+        public override int Height
         {
             set { base.Width = base.Height = value; }
         }
