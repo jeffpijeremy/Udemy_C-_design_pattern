0d5eaab7948c9601dade8d3cccd53d0074352b26
jeffpijeremy <80880882+jeffpijeremy@users.noreply.github.com>
2023-12-28 16:34:27 +0800

    里氏替換原則前會遇到的狀況
    重點在，如果是用
     Rectangle sq = new Square();
    會使 sq.height只有設定height而沒有觸發 square裡面的行為，
    導致指有高有被設定數值，而沒有設定寬
    因為square中是直接new int的方式直接做一個新的實體，後面會教別的作法

---
+++ DesignPatterns/Liskov Substitution Principle/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace Liskov_Substitution_Principle
+{
+    public class Rectangle
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public Rectangle()
+        {
+
+        }
+        public Rectangle(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
+        public override string ToString()
+        {
+            return $"{nameof(Width)}: {Width}, {nameof(Height)}: {Height}";
+        }
+    }
+    public class Square : Rectangle
+    {
+        public new int Width
+        {
+            set { base.Width = base.Height = value; }
+        }
+        public new int Height
+        {
+            set { base.Width = base.Height = value; }
+        }
+    }
+    internal class Program
+    {
+        static public int Area(Rectangle r) => r.Width * r.Height;
+        static void Main(string[] args)
+        {
+            Rectangle rc = new Rectangle(2, 3);
+            Console.WriteLine($"{rc} has area {Area(rc)}");
+
+            //Square sq = new Square();
+
+            Rectangle sq = new Square();
+            sq.Height = 2;
+            Console.WriteLine($"{sq} has area {Area(sq)}");
+        }
+    }
+}
