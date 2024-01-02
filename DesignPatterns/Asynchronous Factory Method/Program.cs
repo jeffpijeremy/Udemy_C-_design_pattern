ad9c86273253d569579fb17dfc524ef9a97c7b7d
jeffpijeremy <80880882+jeffpijeremy@users.noreply.github.com>
2024-01-02 15:51:58 +0800

    非同步工廠模式
    其實就很基本的async去回傳原本的父物件，用途在於比較BUSY的事件行為的後載入跟等候確保機制

---
+++ DesignPatterns/Asynchronous Factory Method/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Asynchronous_Factory_Method
+{
+    public class Foo
+    {
+        public Foo() {
+            
+        }
+        private async Task<Foo> InitAsync()
+        {
+            await Task.Delay(1000);
+            return this;
+        }
+        public static Task<Foo> CreateAsync()
+        {
+            var result = new Foo();
+            return result.InitAsync();
+        }
+    }
+    internal class Program
+    {
+        static async void Main(string[] args)
+        {
+            //var foo = new Foo();
+            //await foo.InitAsync();
+            Foo x = await Foo.CreateAsync();
+        }
+    }
+}
