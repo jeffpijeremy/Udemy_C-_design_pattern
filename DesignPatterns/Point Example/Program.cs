633cb87d429ab001e9f205ef742bbe0ee37f62dc
jeffpijeremy <80880882+jeffpijeremy@users.noreply.github.com>
2024-01-02 15:26:23 +0800

    factory method
    用工廠方法實踐座標設定，
    原本是直接用switch判斷帶入參數去設定，
    改成用呼叫的method去設定，
    特點在於method的擴充性比switch case閱讀來的容易且擴充較為方便

---
+++ DesignPatterns/Point Example/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace Point_Example
+{
+    public enum CoordinateSystem
+    {
+        Cartesian,
+        Polar
+    }
+    public class Point
+    {
+        //factory Method
+        public static Point NewCartesianPoint(double x, double y)
+        {
+            return new Point(x, y);
+        }
+        public static Point NewPolarPoint (double rho,double theta)
+        {
+            return new Point(rho* Math.Cos(theta),rho* Math.Sin(theta));
+        }
+        private double x, y;
+        private Point(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public override string ToString()
+        {
+            return $"{nameof(x)}: {x}, {nameof(y)}: {y}";
+        }
+        #region example for two types point
+        //public Point(double a, double b,CoordinateSystem system = CoordinateSystem.Cartesian)
+        //{
+
+        //    switch (system)
+        //    {
+        //        case CoordinateSystem.Cartesian:
+        //            x = a; y = b; break;
+        //        case CoordinateSystem.Polar:
+        //            x = a * Math.Cos(b);
+        //            y = a * Math.Sin(b); break;
+        //        default:
+        //            throw new ArgumentOutOfRangeException(nameof(system), system, null);
+        //    }
+
+        //}
+        #endregion
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var point = Point.NewPolarPoint(1.0,Math.PI/2);
+            Console.WriteLine(point);
+        }
+    }
+}
